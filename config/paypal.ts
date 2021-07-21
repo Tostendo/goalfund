afc41ce3ac3fb755b7ac710efb749382e2e7d161
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2021-07-21 18:16:48 +0200

    Make paypal client id configurable

--- config/paypal.ts
+++ config/paypal.ts
@@ -1,5 +1,6 @@
 export const PAYPAL_CLIENT_ID = {
   currency: "EUR",
   clientId:
+    process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID ||
     "Ad3HAivb2ehS3QWBsLt-OeQ4d3Fd36Bn-z3nKvm0kR94-DksnKChqzOTvQtByFKjHpxOMaa0EB372uFL",
 };
