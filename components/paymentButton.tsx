e5bc92f9bf995b824aeaaf7befc09f32392c2219
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2021-07-22 19:57:49 +0200

    Try out differrent paypal button

--- components/paymentButton.tsx
+++ components/paymentButton.tsx
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
-import {
-  PayPalScriptProvider,
-  PayPalButtons,
-  FUNDING,
-} from "@paypal/react-paypal-js";
+import { PayPalButton } from "react-paypal-button-v2";
 import { PAYPAL_CLIENT_ID } from "../config/paypal";
 import { useAuth } from "../hooks/useAuth";
 import { createPayment } from "../api/payments";
-import _, { update } from "lodash";
+import _ from "lodash";
 
 type PaymentButtonProps = {
   amount: number;
@@ -19,46 +15,21 @@ const PaymentButton = (props: PaymentButtonProps) => {
   const [paypalErrorMessage, setPaypalErrorMessage] = useState("");
   const auth = useAuth();
 
-  const createOrder = (_: any, actions: any) => {
-    return actions.order
-      .create({
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "EUR",
-              value: props.amount,
-            },
-          },
-        ],
-        // remove the applicaiton_context object if you need your users to add a shipping address
-        application_context: {
-          shipping_preference: "NO_SHIPPING",
-        },
-      })
-      .then((orderID: string) => {
-        return orderID;
-      });
-  };
-
   const getAmount = (details: any) => {
     return parseFloat(
       _.get(details, "purchase_units[0].amount.value", props.amount)
     );
   };
 
-  const onApprove = (_: any, actions: any) => {
-    return actions.order
-      .get()
-      .then(function (details: any) {
-        console.info("details: ", details);
-        const amount = getAmount(details);
-        return createPayment({
-          amount: amount,
-          donationId: props.donationId,
-          payerId: auth?.user?.uid,
-          paid: details.create_time,
-        });
-      })
+  const onSuccess = (details: any, data: any) => {
+    console.info("details: ", details);
+    const amount = getAmount(details);
+    return createPayment({
+      amount: amount,
+      donationId: props.donationId,
+      payerId: auth?.user?.uid,
+      paid: details.create_time,
+    })
       .then(() => {
         console.info("successful payment");
       })
@@ -70,23 +41,26 @@ const PaymentButton = (props: PaymentButtonProps) => {
         setPaypalErrorMessage("Something went wrong.");
       });
   };
+
   return (
-    <PayPalScriptProvider
-      options={{
-        "client-id": PAYPAL_CLIENT_ID.clientId,
-        currency: PAYPAL_CLIENT_ID.currency,
-      }}
-    >
-      <div style={{ maxWidth: "80px", float: "right" }}>
-        <PayPalButtons
-          fundingSource={FUNDING.PAYPAL}
-          style={{ height: 30 }}
-          createOrder={createOrder}
-          onApprove={onApprove}
-        />
-      </div>
+    <div className="w-12 float-right mr-6">
+      <PayPalButton
+        amount={props.amount}
+        currency={PAYPAL_CLIENT_ID.currency}
+        shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+        onSuccess={onSuccess}
+        style={{
+          layout: "horizontal",
+          shape: "pill",
+          height: 30,
+        }}
+        options={{
+          clientId: PAYPAL_CLIENT_ID.clientId,
+          currency: PAYPAL_CLIENT_ID.currency,
+        }}
+      />
       {paypalErrorMessage && <div>{paypalErrorMessage}</div>}
-    </PayPalScriptProvider>
+    </div>
   );
 };
 
