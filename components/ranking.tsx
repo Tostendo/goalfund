6331f9e50b4360ae868889c8047eb64bdcabeec2
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2021-08-16 13:29:38 +0200

    Improve top lists layout

--- components/ranking.tsx
+++ components/ranking.tsx
@@ -52,19 +52,19 @@ const Ranking = ({ type, entries }: RankingProps) => {
     <div className="flex flex-col items-center">
       <div className="py-2 inline-block w-full lg:w-3/4 sm:px-8">
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table className="min-w-full divide-y divide-gray-200">
+          <table className="min-w-full table-fixed divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="w-1/2 sm:w-auto px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase table-cell">
                   Name
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase hidden sm:table-cell">
                   Team
                 </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase hidden sm:table-cell">
                   {getLabels(type).secondary}
                 </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="w-1/2 sm:w-auto px-6 py-3 text-right sm:text-center text-xs font-medium text-gray-500 uppercase table-cell">
                   {getLabels(type).primary}
                 </th>
               </tr>
@@ -73,9 +73,9 @@ const Ranking = ({ type, entries }: RankingProps) => {
               {entries.map((entry) => {
                 return (
                   <tr key={entry.id}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
+                    <td className="px-6 py-4">
+                      <div className="flex flex-col sm:flex-row items-start sm:items-center">
+                        <div className="mr-4 flex-shrink-0 h-10 w-10">
                           <img
                             className="rounded-full"
                             alt="A"
@@ -86,8 +86,8 @@ const Ranking = ({ type, entries }: RankingProps) => {
                             }
                           />
                         </div>
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
+                        <div>
+                          <div className="mt-4 sm:mt-0 text-sm font-medium text-gray-900">
                             <Link href={`/player/${entry.slug}`}>
                               <a>{entry.name}</a>
                             </Link>
@@ -95,7 +95,7 @@ const Ranking = ({ type, entries }: RankingProps) => {
                         </div>
                       </div>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap hidden sm:table-cell">
+                    <td className="px-6 py-4 truncate hidden sm:table-cell">
                       <div className="text-sm text-gray-900">
                         {entry.team && (
                           <Link href={`/teams/${entry.team.slug}`}>
@@ -104,13 +104,13 @@ const Ranking = ({ type, entries }: RankingProps) => {
                         )}
                       </div>
                     </td>
-                    <td className="px-4 py-4 whitespace-nowrap hidden sm:table-cell">
+                    <td className="px-4 py-4 truncate hidden sm:table-cell">
                       <div className="p-2 text-md font-semibold rounded-full text-grey-800 text-center">
                         {getData(type, entry).secondary}
                       </div>
                     </td>
-                    <td className="px-4 py-4 whitespace-nowrap">
-                      <div className="p-2 text-md font-semibold rounded-full text-green-800 text-center">
+                    <td className="px-4 py-4 table-cell">
+                      <div className="p-2 text-md font-semibold rounded-full text-green-800 text-right sm:text-center">
                         {getData(type, entry).primary}
                       </div>
                     </td>
